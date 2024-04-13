9cf64f5425c22c17224507cf381b93eba30c97ff
danielml <daniel@danielml.dev>
2024-04-13 12:58:46 +0200

    Inicializar SyncSpace
    Plantilla obtenida desde Spring Initializr con las siguientes
    dependencias:
    - Spring Boot DevTools
    - Spring Web
    - Thymeleaf
    - Spring Data JPA
    - H2 Database
    - MySQL Driver
    
    Signed-off-by: danielml <daniel@danielml.dev>

---
+++ src/main/java/org/syncspacers/syncspace/SyncspaceApplication.java
@@ -0,0 +1,13 @@
+package org.syncspacers.syncspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncspaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/syncspacers/syncspace/SyncspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.syncspacers.syncspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
